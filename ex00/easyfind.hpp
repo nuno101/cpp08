8a624977fa808045bc5b8552e284eeae5c1390bd
Nuno <nlouro@student.42heilbronn.de>
2022-12-27 16:24:13 +0100

    ex00 with more tests

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 11:50:25 by nlouro            #+#    #+#             */
-/*   Updated: 2022/12/27 15:36:05 by nlouro           ###   ########.fr       */
+/*   Updated: 2022/12/27 16:22:51 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 template <typename T>
 int	easyfind(const T &hay, const int needle)
 {
-	//std::vector<int>::iterator it;
 	typename T::const_iterator it;
 
 	it = find( hay.begin(), hay.end(), needle );
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,23 +6,24 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 11:48:28 by nlouro            #+#    #+#             */
-/*   Updated: 2022/12/27 15:34:12 by nlouro           ###   ########.fr       */
+/*   Updated: 2022/12/27 16:23:09 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
-//#include <map>
+#include <list>
 #include "easyfind.hpp" 
 
 int	main( void )
 {
 	int my_ints[] = {-1, 0, -8, 9, 2};
 	std::vector<int> my_vector ( my_ints, my_ints + 4 );
-	my_vector.push_back(2 );
+	my_vector.push_back(2);
 	my_vector.push_back(33);
 	my_vector.push_back(99);
 
+	std::cout << "Vector tests:" << std::endl;
 	std::cout << "First value [-1]: " << *(my_vector.begin()) << std::endl;
 	std::cout << "First value [-1]: " << my_vector.front() << std::endl;
 	std::cout << "Second value [0]: " << my_vector[1] << std::endl;
@@ -33,11 +34,29 @@ int	main( void )
 	std::cout << "Value -1 present? Found::" << easyfind( my_vector, -1 ) << std::endl;
 	try
 	{
-		std::cout << "Value 99 :" << easyfind( my_vector, 9999 ) << std::endl;
+		std::cout << "Value 9999 :" << easyfind( my_vector, 9999 ) << std::endl;
 	}
 	catch ( std::exception )
 	{
 		
+		std::cout << "Exception caught. Value 9999 not found!\n" << std::endl;
+	}
+
+	std::list<int> my_list;
+	my_list.push_back( -1 );
+	my_list.push_back( 0 );
+	my_list.push_back( -8 );
+	my_list.push_back( 9 );
+
+	std::cout << "List tests:" << std::endl;
+	std::cout << "Value 9 present? Found :" << easyfind( my_list, 9 ) << std::endl;
+	std::cout << "Value -1 present? Found::" << easyfind( my_list, -1 ) << std::endl;
+	try
+	{
+		std::cout << "Value 9999 :" << easyfind( my_list, 9999 ) << std::endl;
+	}
+	catch ( std::exception )
+	{
 		std::cout << "Exception caught. Value 9999 not found!" << std::endl;
 	}
 	return 0;
